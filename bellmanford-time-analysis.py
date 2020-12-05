43626d132681808734475660e39694509985418a
NihaMomin <46839375+NihaMomin@users.noreply.github.com>
2020-12-06 04:10:01 +0500

    Update bellmanford-time-analysis.py

--- bellmanford-time-analysis.py
+++ bellmanford-time-analysis.py
@@ -6,36 +6,25 @@ import matplotlib.pyplot as plt
 class Graph:
 
     def __init__(self, vertices):
-        self.V = vertices  # No. of vertices
+        self.V = vertices
         self.graph = []
-
-    # function to add an edge to graph
     def addEdge(self, u, v, w):
         self.graph.append([u, v, w])
-
-    # utility function used to print the solution
     def printArr(self, dist):
         print("Vertex Distance from Source")
         for i in range(self.V):
             print("{0}\t\t{1}".format(i, dist[i]))
-
     def BellmanFord(self, src):
-
         dist = [float("Inf")] * self.V
         dist[src] = 0
-
         for _ in range(self.V - 1):
-
             for u, v, w in self.graph:
                 if dist[u] != float("Inf") and dist[u] + w < dist[v]:
                     dist[v] = dist[u] + w
-
         for u, v, w in self.graph:
             if dist[u] != float("Inf") and dist[u] + w < dist[v]:
                 print("Graph contains negative weight cycle")
                 return
-
-        # print all distance
         self.printArr(dist)
 # g = Graph(150)
 # matrix = np.random.randint(5, size=(150,150))
